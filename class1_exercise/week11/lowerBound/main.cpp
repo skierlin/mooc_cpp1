5cafe4e766239058583ee6e1cc65d3c4f1fb6982
skier_lin <663250456@qq.com>
2019-08-27 17:10:54 +0800

    add content of week11

---
+++ class1_exercise/week11/lowerBound/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution{
+	
+public:
+	int BinarySearch(vector<int>& in,int p){
+		int l = 0;
+		int r = in.size()-1;
+		int index = -1;
+		while(l <= r){
+			int mid = l + (r-l)/2;
+			if(p <= in[mid])
+				r = mid - 1;
+			else{
+				index = mid;
+				l = mid + 1;
+			}
+		}
+		return index;
+	}
+};
+
+
+int main(){
+	
+	vector<int> in = {4,5,3,1,2};
+	sort(in.begin(),in.end());
+	
+	Solution res;
+	int n = res.BinarySearch(in,4);
+
+	if(n != -1)
+		cout << "i find it and the number is " << n << endl;
+	else
+		cout << "none" << endl;
+
+	cout << endl;
+	return 0;
+}
